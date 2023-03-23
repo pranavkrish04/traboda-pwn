ebeb74a85c9aa0ae5448ffd33f2dddbd67e98939
tourpran <thepranavkrish04@gmail.com>
2023-03-23 22:48:20 +0530

    done

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget https://github.com/pranavkrish04/traboda-pwn/blob/main/chall?raw=true -O chall
-
+wget https://raw.githubusercontent.com/pranavkrish04/traboda-pwn/main/gdbcommands
 chmod +x chall
-gdb chall
+gdb chall -x gdbcommands
